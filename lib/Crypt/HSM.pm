4bf16cbeed5d4e1667c5d5d9930fa712cd3dc59f
Leon Timmermans <fawaka@gmail.com>
2023-10-20 00:47:49 +0200

    Split Crypt::HSM::Provider out of Crypt::HSM namespace

--- lib/Crypt/HSM.pm
+++ lib/Crypt/HSM.pm
@@ -6,13 +6,6 @@ use warnings;
 use XSLoader;
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
-#backwards compat
-sub open_session {
-	my ($self, $slot, @args) = @_;
-	my $object = ref($slot) ? $slot : $self->slot($slot);
-	return $object->open_session(@args);
-}
-
 1;
 
 #ABSTRACT: A PKCS11 interface for Perl
@@ -33,20 +26,4 @@ This module interfaces with any PKCS11 library to use its cryptography.
 
 =method load($path)
 
-This loads the pkcs11 found a $path, and returns it as a new Crypt::HSM object.
-
-=method slots($available = 1)
-
-This lists the slots of this interface as L<Crypt::HSM::Slot|Crypt::HSM::Slot>. If C<$available> is true only slots with a token available will be listed.
-
-=method slot($identifier)
-
-This returns a L<Crypt::HSM::Slot|Crypt::HSM::Slot> for the slot with the given identifier.
-
-=method info()
-
-This returns a hash with information about the HSM.
-
-=method open_session($slot, $flags)
-
-This methods wraps around C<Crypt::HSM::Slot>'s C<open_session> method [depreciated].
+This loads the pkcs11 found a $path, and returns it as a new L<Crypt::HSM::Provider|Crypt::HSM::Provider> object.
---
+++ lib/Crypt/HSM/Provider.pm
@@ -0,0 +1,31 @@
+package Crypt::HSM::Provider;
+
+use strict;
+use warnings;
+
+1;
+
+#ABSTRACT: A PKCS11 provider
+
+=head1 SYNOPSIS
+
+ my $hsm = Crypt::HSM->load('/usr/lib/pkcs11/libsofthsm2.so');
+ my ($slot) = $hsm->slots;
+ my $session = $slot->open_session;
+ $session->login('user', '1234');
+
+=head1 DESCRIPTION
+
+This module interfaces with any PKCS11 interface to use its cryptography.
+
+=method slots($available = 1)
+
+This lists the slots of this interface as L<Crypt::HSM::Slot|Crypt::HSM::Slot>. If C<$available> is true only slots with a token available will be listed.
+
+=method slot($identifier)
+
+This returns a L<Crypt::HSM::Slot|Crypt::HSM::Slot> for the slot with the given identifier.
+
+=method info()
+
+This returns a hash with information about the HSM.
