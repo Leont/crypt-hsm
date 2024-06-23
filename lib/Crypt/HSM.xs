Leont/crypt-hsm

commit 0b0631f692c66c2cb55f9243347b1fe78e2839c3

Switch Slot and Mechanism objects to T_MAGICEXT

lib/Crypt/HSM.xs
@@ -1513,19 +1513,33 @@ static int provider_free(pTHX_ SV* sv, MAGIC* magic) {
 
 static const MGVTBL Crypt__HSM_magic = { NULL, NULL, NULL, NULL, provider_free, NULL, provider_dup, NULL };
 
+static int provider_ptr_dup(pTHX_ MAGIC* magic, CLONE_PARAMS* params) {
+	PERL_UNUSED_VAR(params);
+	provider_refcount_increment(*(struct Provider**)magic->mg_ptr);
+	return 0;
+}
+
+static int provider_ptr_free(pTHX_ SV* sv, MAGIC* magic) {
+	PERL_UNUSED_VAR(sv);
+	provider_refcount_decrement(*(struct Provider**)magic->mg_ptr);
+	return 0;
+}
+
 struct Slot {
 	struct Provider* provider;
 	CK_SLOT_ID slot;
 };
 typedef struct Slot* Crypt__HSM__Slot;
 
+static const MGVTBL Crypt__HSM__Slot_magic = { NULL, NULL, NULL, NULL, provider_ptr_free, NULL, provider_ptr_dup, NULL };
+
 static SV* S_new_slot(pTHX_ struct Provider* provider, CK_SLOT_ID slot) {
-	struct Slot* entry;
-	Newxz(entry, 1, struct Slot);
+	struct Slot* entry = PerlMemShared_calloc(1, sizeof(struct Slot*));
 	entry->slot = slot;
 	entry->provider = provider_refcount_increment(provider);
 	SV* object = newSV(0);
-	sv_magic(newSVrv(object, "Crypt::HSM::Slot"), NULL, PERL_MAGIC_ext, (const char*)entry, 0);
+	MAGIC* magic = sv_magicext(newSVrv(object, "Crypt::HSM::Slot"), NULL, PERL_MAGIC_ext, &Crypt__HSM__Slot_magic, (const char*)entry, 0);
+	magic->mg_flags = MGf_DUP;
 	return object;
 }
 #define new_slot(provider, slot) S_new_slot(aTHX_ provider, slot)
@@ -1539,14 +1553,16 @@ struct Mechanism {
 };
 typedef struct Mechanism* Crypt__HSM__Mechanism;
 
+static const MGVTBL Crypt__HSM__Mechanism_magic = { NULL, NULL, NULL, NULL, provider_ptr_free, NULL, provider_ptr_dup, NULL };
+
 static SV* S_new_mechanism(pTHX_ struct Provider* provider, CK_SLOT_ID slot, CK_MECHANISM_TYPE mechanism) {
-	struct Mechanism* entry;
-	Newxz(entry, 1, struct Mechanism);
+	struct Mechanism* entry = PerlMemShared_calloc(1, sizeof(struct Mechanism*));
 	entry->mechanism = mechanism;
 	entry->slot = slot;
 	entry->provider = provider_refcount_increment(provider);
 	SV* object = newSV(0);
-	sv_magic(newSVrv(object, "Crypt::HSM::Mechanism"), NULL, PERL_MAGIC_ext, (const char*)entry, 0);
+	MAGIC* magic = sv_magicext(newSVrv(object, "Crypt::HSM::Mechanism"), NULL, PERL_MAGIC_ext, &Crypt__HSM__Mechanism_magic, (const char*)entry, 0);
+	magic->mg_flags = MGf_DUP;
 	return object;
 }
 #define new_mechanism(provider, slot, mechanism) S_new_mechanism(aTHX_ provider, slot, mechanism)
@@ -1702,10 +1718,6 @@ OUTPUT:
 
 MODULE = Crypt::HSM	 PACKAGE = Crypt::HSM::Slot
 
-void DESTROY(Crypt::HSM::Slot self)
-CODE:
-	provider_refcount_decrement(self->provider);
-
 CK_SLOT_ID id(Crypt::HSM::Slot self)
 CODE:
 	RETVAL = self->slot;
@@ -1817,10 +1829,6 @@ CODE:
 
 MODULE = Crypt::HSM  PACKAGE = Crypt::HSM::Mechanism
 
-
-void DESTROY(Crypt::HSM::Mechanism self)
-CODE:
-	provider_refcount_decrement(self->provider);
 
 const char* name(Crypt::HSM::Mechanism self)
 CODE:

typemap
@@ -1,7 +1,7 @@
 TYPEMAP
 	Crypt::HSM              T_MAGICEXT
-	Crypt::HSM::Slot        T_MAGIC
-	Crypt::HSM::Mechanism   T_MAGIC
+	Crypt::HSM::Slot        T_MAGICEXT
+	Crypt::HSM::Mechanism   T_MAGICEXT
 	Crypt::HSM::Session     T_MAGIC
 	Crypt::HSM::Stream      T_MAGIC
 	Crypt::HSM::Encrypt     T_MAGIC

commit 23e4a0b8c3a889a9b322812e93b48f50b19a1bca

Fixed using Mechanism object as mechanism argument
This fixes a regression in 0b0631f692c66c2cb55f9243347b1fe78e2839c3

lib/Crypt/HSM.xs
@@ -1604,8 +1604,10 @@ static SV* S_new_mechanism(pTHX_ struct Provider* provider, CK_SLOT_ID slot, CK_
 
 static CK_MECHANISM_TYPE S_get_mechanism_type(pTHX_ SV* input) {
 	if (SvROK(input) && sv_derived_from(input, "Crypt::HSM::Mechanism")) {
-		IV tmp = SvIV(SvRV(input));
-		struct Mechanism* mech = INT2PTR(struct Mechanism*, tmp);
+		MAGIC* magic = mg_findext(SvRV(input), PERL_MAGIC_ext, &Crypt__HSM__Mechanism_magic);
+		if (!magic)
+			Perl_croak(aTHX_ "No magic found on Crypt::HSM::Mechanism object");
+		struct Mechanism* mech = (struct Mechanism*) magic->mg_ptr;
 		return mech->mechanism;
 	} else {
 		return map_get(mechanisms, input, "mechanism");